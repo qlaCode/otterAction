a7098f4c10f1ea17eef5c6488d4668221110bedc
Quentin Lamare <1092-qlaCode@users.noreply.gitlab.propulsion-home.ch>
2024-03-12 22:07:50 +0100

    Added responsive navbar; adjusted styling

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMobileBar(){
+    const mobilebar = document.querySelector('.mobilebar');
+    mobilebar.style.display = 'flex';
+
+};
+
+function hideMobileBar(){
+    const mobilebar = document.querySelector('.mobilebar');
+    mobilebar.style.display = 'none';
+};
